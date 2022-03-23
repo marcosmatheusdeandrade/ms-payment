deec6bbdcc48267c0c0e11c5e1664ea15b84b718
Marcos Matheus de Andrade <mmatheusandrade2013@gmail.com>
2022-03-22 21:08:56 -0300

    Criada entidade worker e ajustadas configurações iniciais do projeto

---
+++ hr-worker/src/main/java/br/com/mma/resources/WorkerController.java
@@ -0,0 +1,33 @@
+package br.com.mma.resources;
+
+import br.com.mma.entities.Worker;
+import br.com.mma.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerController {
+
+    private WorkerRepository repository;
+
+    @Autowired
+    public WorkerController(WorkerRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(Long id) {
+        return ResponseEntity.ok(repository.findById(id).get());
+    }
+}
