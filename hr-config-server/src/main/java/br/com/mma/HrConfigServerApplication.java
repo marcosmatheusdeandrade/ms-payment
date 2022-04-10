24f5c05babe18cec824e7e17baff2448b9d16ded
Marcos Matheus de Andrade <mmatheusandrade2013@gmail.com>
2022-04-10 20:51:00 -0300

    Criado projeto hr-config-server

---
+++ hr-config-server/src/main/java/br/com/mma/HrConfigServerApplication.java
@@ -0,0 +1,12 @@
+package br.com.mma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrConfigServerApplication .class, args);
+    }
+}
