171e73054048d2ea941fe5a24b5118b58dea08a5
Marcos Matheus de Andrade <mmatheusandrade2013@gmail.com>
2022-04-23 00:02:56 -0300

    Ler configuração remota de outro repositório

--- hr-worker/src/main/java/br/com/mma/resources/WorkerController.java
+++ hr-worker/src/main/java/br/com/mma/resources/WorkerController.java
@@ -5,6 +5,7 @@ import br.com.mma.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,9 @@ public class WorkerController {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
@@ -33,6 +37,12 @@ public class WorkerController {
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> findConfigs() {
+        logger.info("CONFIG = "+testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{id}")
